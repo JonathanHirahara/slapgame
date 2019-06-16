9ff7e0d59b00a1466a4d78d2152656ee05960cbb
JonathanHirahara <jdhirahara@gmail.com>
2019-06-16 13:57:46 -0600

    stuck

--- game.js
+++ game.js
@@ -1,52 +1,48 @@
-let Steve = {
+let opponentOne = {
+  name: "Steve",
   hitPoints: 20,
-
 }
-let Ralph = {
+let opponentTwo = {
+  name: "Ralph",
   hitPoints: 23,
 }
 
 let strikes = 0;
-
-
 let hpElem = document.querySelector("#life")
-
 let hitsElem = document.querySelector("#strikes")
 
+function slap() {
+  opponentTwo.hitPoints -= 1
+  strikes += 1
+  hitsElem.innerHTML = strikes.toString()
+  hpElem.innerHTML = opponentTwo.hitPoints.toString()
+  death()
+}
+function punch() {
+  opponentTwo.hitPoints -= 3
+  strikes += 1
+  hitsElem.innerHTML = strikes.toString()
+  hpElem.innerHTML = opponentTwo.hitPoints.toString()
+  death()
+}
+function stomp() {
+  opponentTwo.hitPoints -= 2
+  strikes += 1
+  hitsElem.innerHTML = strikes.toString()
+  hpElem.innerHTML = opponentTwo.hitpoints.toString()
+  death()
+}
+function death() {
+  if (opponentTwo.hitPoints <= 0) {
+    alert("opponent is dead")
+    update()
+  }
+}
 function update() {
   strikes = 0
 
   hitsElem.innerHTML = strikes.toString()
-  hpElem.innerHTML = Ralph.hitPoints.toString()
+  hpElem.innerHTML = opponentTwo.hitPoints.toString()
 }
 
-function slap() {
-  Ralph.hitPoints -= 1
-  strikes += 1
-  hitsElem.innerHTML = strikes.toString()
-  hpElem.innerHTML = Ralph.hitPoints.toString()
-  death()
-}
-
-function punch() {
-  Ralph.hitPoints -= 3
-  strikes += 1
-  hitsElem.innerHTML = strikes.toString()
-  hpElem.innerHTML = Ralph.hitPoints.toString()
-  death()
-}
-function stomp() {
-  Ralph.hitPoints -= 2
-  strikes += 1
-  hitsElem.innerHTML = strikes.toString()
-  hpElem.innerHTML = Ralph.hitpoints.toString()
-  death()
-}
-
-
-function death() {
-  if (Ralph.hitPoints <= 0) {
-    alert("opponent is dead")
-    update()
-  }
-} update()
+update()
