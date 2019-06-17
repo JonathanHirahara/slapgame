4aa89365131a462e80d4b87144364b9099e5d5c7
JonathanHirahara <jdhirahara@gmail.com>
2019-06-17 15:15:36 -0600

    aha finally

--- game.js
+++ game.js
@@ -1,71 +1,101 @@
+let opponent;
+
 let enemyOne =
 {
   name: "Steve",
-  hp: 12
+  hp: 12,
+  hits: 0,
+  items: [],
 }
 let enemyTwo = {
   name: "Dave",
-  hp: 15
+  hp: 15,
+  hits: 0,
+  items: []
 }
-let item =
-{
-  name: "strength stim",
-  effect: 2,
+let availableItems = {
+  stoneFist: {
+    effect: 3,
+  },
+  knifeHand: {
+    effect: 1
+  },
+  leadFoot: {
+    effect: 2
+  }
 }
 
-let opponent;
-let hp = 0;
-let hits = 0
+
 let hpElem = document.querySelector("#life")
 let hitsElem = document.querySelector("#strikes")
-hitsElem.innerHTML = hits.toString()
-hpElem.innerHTML = hp.toString()
+
 
 function choose(i) {
-  switch (i) {
-    case 1:
-      opponent = enemyOne
-      hp = enemyOne.hp
-      hitsElem.innerHTML = hits.toString()
-      hpElem.innerHTML = hp.toString()
-      break;
-    case 2:
-      opponent = enemyTwo
-      hp = enemyTwo.hp
-      hitsElem.innerHTML = hits.toString()
-      hpElem.innerHTML = hp.toString()
-      break;
+  if (!opponent) {
+
+
+    switch (i) {
+      case 1:
+        opponent = enemyOne
+        break;
+      case 2:
+        opponent = enemyTwo
+        break;
+    }
+    hitsElem.innerHTML = opponent.hits.toString()
+    hpElem.innerHTML = opponent.hp.toString()
   }
 }
 
 function hit(i) {
+  if (opponent.hp == 0) {
+    return
+  }
+
   switch (i) {
     case 1:
-      hp -= 3
+      opponent.hp -= 3 + addMods()
       break;
     case 2:
-      hp -= 1
+      opponent.hp -= 1 + addMods()
       break;
     case 3:
-      hp -= 5
-  } hits += 1
+      opponent.hp -= 5 + addMods()
+  }
+  opponent.hits += 1
 
 
-  hitsElem.innerHTML = hits.toString()
-  hpElem.innerHTML = hp.toString()
-  death()
+  hitsElem.innerHTML = opponent.hits.toString()
+  hpElem.innerHTML = opponent.hp.toString()
+  update()
 }
 
-function death() {
-  if (hp <= 0) {
-    alert("opponent is dead")
-    update()
+
+
+function update() {
+  if (opponent.hp <= 0) {
+    alert("opponent is dead, choose another")
+
+    window.location.reload()
+  }
+  hitsElem.innerHTML = opponent.hits.toString()
+  hpElem.innerHTML = opponent.hp.toString()
+}
+
+
+
+function giveItem(item) {
+  if (!opponent.items.length) {
+    opponent.items.push(availableItems[item])
   }
 }
-function update() {
-  hits = 0
-  hp = 0
-  hitsElem.innerHTML = hits.toString()
-  hpElem.innerHTML = hp.toString()
-}
 
+function addMods() {
+  let effectTotal = 0
+  for (let index = 0; index < opponent.items.length; index++) {
+    let item = opponent.items[index];
+    effectTotal += item.effect
+  }
+  return effectTotal
+
+}
