e39e0cc3358d34a9ca2fc45c2af7de70c92bb712
JonathanHirahara <jdhirahara@gmail.com>
2019-06-17 10:41:09 -0600

    finally

--- game.js
+++ game.js
@@ -1,48 +1,71 @@
-let opponentOne = {
+let enemyOne =
+{
   name: "Steve",
-  hitPoints: 20,
+  hp: 12
 }
-let opponentTwo = {
-  name: "Ralph",
-  hitPoints: 23,
+let enemyTwo = {
+  name: "Dave",
+  hp: 15
+}
+let item =
+{
+  name: "strength stim",
+  effect: 2,
 }
 
-let strikes = 0;
+let opponent;
+let hp = 0;
+let hits = 0
 let hpElem = document.querySelector("#life")
 let hitsElem = document.querySelector("#strikes")
+hitsElem.innerHTML = hits.toString()
+hpElem.innerHTML = hp.toString()
 
-function slap() {
-  opponentTwo.hitPoints -= 1
-  strikes += 1
-  hitsElem.innerHTML = strikes.toString()
-  hpElem.innerHTML = opponentTwo.hitPoints.toString()
+function choose(i) {
+  switch (i) {
+    case 1:
+      opponent = enemyOne
+      hp = enemyOne.hp
+      hitsElem.innerHTML = hits.toString()
+      hpElem.innerHTML = hp.toString()
+      break;
+    case 2:
+      opponent = enemyTwo
+      hp = enemyTwo.hp
+      hitsElem.innerHTML = hits.toString()
+      hpElem.innerHTML = hp.toString()
+      break;
+  }
+}
+
+function hit(i) {
+  switch (i) {
+    case 1:
+      hp -= 3
+      break;
+    case 2:
+      hp -= 1
+      break;
+    case 3:
+      hp -= 5
+  } hits += 1
+
+
+  hitsElem.innerHTML = hits.toString()
+  hpElem.innerHTML = hp.toString()
   death()
 }
-function punch() {
-  opponentTwo.hitPoints -= 3
-  strikes += 1
-  hitsElem.innerHTML = strikes.toString()
-  hpElem.innerHTML = opponentTwo.hitPoints.toString()
-  death()
-}
-function stomp() {
-  opponentTwo.hitPoints -= 2
-  strikes += 1
-  hitsElem.innerHTML = strikes.toString()
-  hpElem.innerHTML = opponentTwo.hitpoints.toString()
-  death()
-}
+
 function death() {
-  if (opponentTwo.hitPoints <= 0) {
+  if (hp <= 0) {
     alert("opponent is dead")
     update()
   }
 }
 function update() {
-  strikes = 0
-
-  hitsElem.innerHTML = strikes.toString()
-  hpElem.innerHTML = opponentTwo.hitPoints.toString()
+  hits = 0
+  hp = 0
+  hitsElem.innerHTML = hits.toString()
+  hpElem.innerHTML = hp.toString()
 }
 
-update()
